2f18f1705ed98e764afc02b35f245fbc09dc2525
Arie Septian <ariesptn@gmail.com>
2019-01-29 18:57:30 +0700

    routes

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
+'use strict'
 const model = require('./models')
+const routes = require('./routes')
 
+const express = require('express')
+const app = express()
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static('public'))
+
+
+app.use('/', routes)
+
+app.listen(3000)
